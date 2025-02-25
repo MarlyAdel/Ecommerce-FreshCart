340a44036a66bd53359f188e6b671a4a415d87ab
MarlyAdel <marlooadel1100@gmail.com>
2025-02-25 03:38:44 +0200

    react

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
