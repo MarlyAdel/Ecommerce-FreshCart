0964358653112a067bc681312545421df50282c4
MarlyAdel <marlooadel1100@gmail.com>
2025-02-25 22:56:16 +0200

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
       {path:"cart", element: <ProtectedRoutes><Cart/></ProtectedRoutes> },
       {path:"productDetails/:id/:categoryId", element: <ProtectedRoutes><ProductDetails/></ProtectedRoutes> },
       {path:"checkout", element: <ProtectedRoutes><Checkout/></ProtectedRoutes> },
-      {path:"/allorders", element: <ProtectedRoutes><AllOrders/></ProtectedRoutes> },
+      {path:"/allorders", element: <AuthView><AllOrders/></AuthView> },
 
       {path:"/forgot-password", element: <AuthView><ForgotPassword/></AuthView> },
       {path:"/verify-code", element: <AuthView><VerifyCode/></AuthView> },
